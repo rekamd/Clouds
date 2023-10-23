92fb70b2276589bd8e5aef6bc5cfb0bd2dd46733
rekamd <64975845+rekamd@users.noreply.github.com>
2023-10-23 13:43:18 +0000

    More randomness in shapes

--- src/shaders.js
+++ src/shaders.js
@@ -156,7 +156,7 @@ export const cloudFragmentShader = /* glsl */ `
         float cloudHash = float(c+1);
         vec3 cloudPositionCloud = cloudPosition + random3D(cloudHash, baseSeed) * cloudOffset;
 
-        float cloudShift = max(0.1,abs(random(cloudHash + baseSeed))) * maxShiftSpeed * time + random(cloudHash + baseSeed + 37.2) * 3287.102;
+        float cloudShift = max(0.1,abs(random(cloudHash, baseSeed))) * maxShiftSpeed * time + random(cloudHash, baseSeed + 37.2) * 3287.102;
         cloudShift = mod(cloudShift, skyCutoffDistance*2.0);
         float transitionZone = dot(cloudSize, cloudShiftDirection);
         float alphaBegin = tanh(cloudShift/transitionZone);
@@ -166,7 +166,7 @@ export const cloudFragmentShader = /* glsl */ `
         cloudPositionCloud += (cloudShift - skyCutoffDistance) * cloudShiftDirection;
     
         float randomTime = randShiftAndScale(time, maxTimeScale, maxTimeShift, timeSeed + cloudHash);
-        float depth = cloudDepth(cloudPositionCloud, invAlpha * invCloudSize, cloudScatter, cloudShape, cloudRoughness, randomTime);
+        float depth = cloudDepth(cloudPositionCloud, invAlpha * invCloudSize, cloudScatter, cloudShape + 17.213 * random(cloudHash, baseSeed), cloudRoughness, randomTime);
 
         maxDepth = max(depth, maxDepth);
       }
@@ -182,7 +182,7 @@ export const cloudFragmentShader = /* glsl */ `
           float cloudHash = float(c+1);
           vec3 lightPositionCloud = lightPosition + random3D(cloudHash, baseSeed) * cloudOffset;
 
-          float cloudShift = max(0.1,abs(random(cloudHash + baseSeed))) * maxShiftSpeed * time + random(cloudHash + baseSeed + 37.2) * 3287.102;
+          float cloudShift = max(0.1,abs(random(cloudHash, baseSeed))) * maxShiftSpeed * time + random(cloudHash, baseSeed + 37.2) * 3287.102;
           cloudShift = mod(cloudShift, skyCutoffDistance*2.0);
           vec3 cloudShiftDirection = vec3(1,0,0);
           lightPositionCloud += (cloudShift - skyCutoffDistance) * cloudShiftDirection;
@@ -192,7 +192,7 @@ export const cloudFragmentShader = /* glsl */ `
           float shadow = 0.0;
           for (float s = 0.0; s < uShadowSteps; s++) {
             lightPositionCloud += lightDirection * shadowStepLength;
-            shadow += cloudDepth(lightPositionCloud, invCloudSize, cloudScatter, cloudShape, cloudRoughness, randomTime);
+            shadow += cloudDepth(lightPositionCloud, invCloudSize, cloudScatter, cloudShape + 17.213 * random(cloudHash, baseSeed), cloudRoughness, randomTime);
           }
           shadow = exp((-shadow / uShadowSteps) * 3.0);
           minShadow = min(shadow, minShadow);
