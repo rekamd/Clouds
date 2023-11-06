6ba9a3fc1ed5a838601fb796535f5120598877ae
rekamd <64975845+rekamd@users.noreply.github.com>
2023-11-06 20:44:35 +0000

    Add artblocks tokenData test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,44 @@ import Stats from "three/examples/jsm/libs/stats.module.js";
 import Cloud from "./Cloud";
 import { Timer } from "./Timer.js";
 
+let tokenTest = false;
+
+// generate token data in the right format as done in the Artblocks template
+function genTokenData(projectNum) {
+  let data = {};
+  let hash = "0x";
+  for (var i = 0; i < 64; i++) {
+    hash += Math.floor(Math.random() * 16).toString(16);
+  }
+  data.hash = hash;
+  data.tokenId = (
+    projectNum * 1000000 +
+    Math.floor(Math.random() * 1000)
+  ).toString();
+  return data;
+}
+// provide global tokenData variable as in Artblocks environment
+let tokenData = genTokenData(99);
+
+// Turn the hash string, containing 64 hexadecimal numbers in sequence, into hash pairs of 2 hexadecimal numbers.
+// This means 0xeca4cf6288eb455f388301c28ac01a8da5746781d22101a65cb78a96a49512c8
+// turns into ["ec", "a4", "cf", "62", "88", "eb", ...]
+const hashPairs = [];
+for (let j = 0; j < 32; j++) {
+  hashPairs.push(tokenData.hash.slice(2 + j * 2, 4 + j * 2));
+}
+
+// convert hash pairs into individual integer hash values ranging from 0 to 255
+const hashValuesInt = hashPairs.map((x) => {
+  return parseInt(x, 16);
+});
+
+// create normalized floating point hash values ranging from 0.0 to 1.0
+const hashValuesNorm = [];
+for (let j = 0; j < 32; j++) {
+  hashValuesNorm.push(hashValuesInt[j] / 255.0);
+}
+
 let stats = new Stats();
 document.body.appendChild(stats.dom);
 
@@ -28,11 +66,27 @@ controls.enableDamping = true;
 let timer = new Timer();
 timer.enableFixedDelta();
 
+let cloudSeed = 83.0;
+let sunPositionX = 4.0;
+let sunPositionY = 3.5;
+let sunPositionZ = -1.0;
+
+// assign random values based on tokenData hash
+if (tokenTest) {
+  cloudSeed = hashValuesInt[0];
+  let maxSunPosXZ = 10.0;
+  let minSunPosY = 2.0;
+  let maxSunPosY = 10.0;
+  sunPositionX = -maxSunPosXZ + 2 * maxSunPosXZ * hashValuesNorm[1];
+  sunPositionY = minSunPosY + (maxSunPosY - minSunPosY) * hashValuesNorm[2];
+  sunPositionZ = -maxSunPosXZ + 2 * maxSunPosXZ * hashValuesNorm[3];
+}
+
 let params = {
   skyColor: 0x337fff,
-  sunPositionX: 4.0,
-  sunPositionY: 3.5,
-  sunPositionZ: -1.0,
+  sunPositionX: sunPositionX, //4.0,
+  sunPositionY: sunPositionY, //3.5,
+  sunPositionZ: sunPositionZ, //-1.0,
   cloudColor: 0xeabf6b,
   uniformPixels: true,
   lastTouchedPixelID: 0,
@@ -41,10 +95,11 @@ let params = {
 };
 
 let cloud = new Cloud(camera, {
-  cloudSeed: 83.0,
+  cloudSeed: cloudSeed, //83.0,
   cloudCount: 8,
   cloudSize: new THREE.Vector3(2, 1, 2),
   sunIntensity: 1.0,
+  sunPosition: new THREE.Vector3(sunPositionX, sunPositionY, sunPositionZ),
   cloudColor: new THREE.Color(params.cloudColor), //"rgb(234, 191, 107)"
   skyColor: new THREE.Color(params.skyColor), //"rgb(51, 127, 255)"
   cloudSteps: 48,
